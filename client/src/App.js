5bc69e4c1c347a2bc26b87a53295f72bda11a6ac
Niki Balajadia <niki@Nikis-MacBook-Pro.local>
2022-06-21 15:45:48 -1000

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link, Route, Swtich } from 'react-router-dom'
 
 import SavedList from './Movies/SavedList';
 
-export default function App () {
+export default function App() {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
 
